1ab75f5a3971bc5e5ca0dd7d31bae37c20fbd810
Huynh Bach <huynh.bach@lingkail.com>
2021-06-27 21:09:49 +0800

    first push

---
+++ loginapp/loginapp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Reflection;
+namespace loginapp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            var assembly = typeof(ImageSource).GetTypeInfo().Assembly;
+            foreach (var res in assembly.GetManifestResourceNames())
+            {
+                System.Diagnostics.Debug.WriteLine("found resource: " + res);
+            }
+        }
+    }
+
+
+        
+    
+}
